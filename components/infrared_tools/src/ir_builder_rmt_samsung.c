68d58e8b11b86c033f717aa32ac6cbba699ecf88
Sohan Chaudhary <soc2053@gmail.com>
2022-09-15 08:05:57 +0545

    First commit

---
+++ components/infrared_tools/src/ir_builder_rmt_samsung.c
@@ -0,0 +1,206 @@
+/*******************
+SAMSUNG Protocol
+http://techiesms.blogspot.com/2016/01/ir-protocol-decoding-and-transmiting.html
+1 bit start bit
+8 bit Address
+8 bit address
+8 bit data
+inverse of 8 bit
+stop bit
+
+start bit - Address - Address - Data - Inverse Data - stop bit
+********************/
+#include <sys/cdefs.h>
+#include "esp_log.h"
+#include "ir_tools.h"
+#include "ir_timings.h"
+#include "driver/rmt.h"
+
+static const char *TAG = "samsung_builder";
+#define SAMSUNG_CHECK(a, str, goto_tag, ret_value, ...)                               \
+    do                                                                            \
+    {                                                                             \
+        if (!(a))                                                                 \
+        {                                                                         \
+            ESP_LOGE(TAG, "%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+            ret = ret_value;                                                      \
+            goto goto_tag;                                                        \
+        }                                                                         \
+    } while (0)
+
+typedef struct {
+    ir_builder_t parent;
+    uint32_t buffer_size;
+    uint32_t cursor;
+    uint32_t flags;
+    uint32_t leading_code_high_ticks;
+    uint32_t leading_code_low_ticks;
+    uint32_t repeat_code_high_ticks;
+    uint32_t repeat_code_low_ticks;
+    uint32_t payload_logic0_high_ticks;
+    uint32_t payload_logic0_low_ticks;
+    uint32_t payload_logic1_high_ticks;
+    uint32_t payload_logic1_low_ticks;
+    uint32_t ending_code_high_ticks;
+    uint32_t ending_code_low_ticks;
+    bool inverse;
+    rmt_item32_t buffer[];
+} samsung_builder_t;
+
+//code to edit
+static esp_err_t samsung_builder_make_head(ir_builder_t *builder)
+{
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    samsung_builder->cursor = 0;
+    samsung_builder->buffer[samsung_builder->cursor].level0 = !samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration0 = samsung_builder->leading_code_high_ticks;
+    samsung_builder->buffer[samsung_builder->cursor].level1 = samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration1 = samsung_builder->leading_code_low_ticks;
+    samsung_builder->cursor += 1;
+    return ESP_OK;
+}
+
+static esp_err_t samsung_builder_make_logic0(ir_builder_t *builder)
+{
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    samsung_builder->buffer[samsung_builder->cursor].level0 = !samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration0 = samsung_builder->payload_logic0_high_ticks;
+    samsung_builder->buffer[samsung_builder->cursor].level1 = samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration1 = samsung_builder->payload_logic0_low_ticks;
+    samsung_builder->cursor += 1;
+    return ESP_OK;
+}
+
+static esp_err_t samsung_builder_make_logic1(ir_builder_t *builder)
+{
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    samsung_builder->buffer[samsung_builder->cursor].level0 = !samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration0 = samsung_builder->payload_logic1_high_ticks;
+    samsung_builder->buffer[samsung_builder->cursor].level1 = samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration1 = samsung_builder->payload_logic1_low_ticks;
+    samsung_builder->cursor += 1;
+    return ESP_OK;
+}
+
+static esp_err_t samsung_builder_make_end(ir_builder_t *builder)
+{
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    samsung_builder->buffer[samsung_builder->cursor].level0 = !samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration0 = samsung_builder->ending_code_high_ticks;
+    samsung_builder->buffer[samsung_builder->cursor].level1 = samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration1 = samsung_builder->ending_code_low_ticks;
+    samsung_builder->cursor += 1;
+    samsung_builder->buffer[samsung_builder->cursor].val = 0;
+    samsung_builder->cursor += 1;
+    return ESP_OK;
+}
+
+static esp_err_t samsung_build_frame(ir_builder_t *builder, uint32_t address, uint32_t command)
+{
+    esp_err_t ret = ESP_OK;
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    if (!samsung_builder->flags & IR_TOOLS_FLAGS_PROTO_EXT) {
+        uint8_t low_byte = address & 0xFF;
+        uint8_t high_byte = (address >> 8) & 0xFF;
+        SAMSUNG_CHECK(low_byte == (high_byte & 0xFF), "address not match standard SAMSUNG protocol", err, ESP_ERR_INVALID_ARG);
+        low_byte = command & 0xFF;
+        high_byte = (command >> 8) & 0xFF;
+        SAMSUNG_CHECK(low_byte == (~high_byte & 0xFF), "command not match standard SAMSUNG protocol", err, ESP_ERR_INVALID_ARG);
+    }
+    builder->make_head(builder);
+    // LSB -> MSB
+    for (int i = 0; i < 16; i++) {
+        if (address & (1 << i)) {
+            builder->make_logic1(builder);
+        } else {
+            builder->make_logic0(builder);
+        }
+    }
+    for (int i = 0; i < 16; i++) {
+        if (command & (1 << i)) {
+            builder->make_logic1(builder);
+        } else {
+            builder->make_logic0(builder);
+        }
+    }
+    builder->make_end(builder);
+    return ESP_OK;
+err:
+    return ret;
+}
+
+static esp_err_t samsung_build_repeat_frame(ir_builder_t *builder)
+{
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    samsung_builder->cursor = 0;
+    samsung_builder->buffer[samsung_builder->cursor].level0 = !samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration0 = samsung_builder->repeat_code_high_ticks;
+    samsung_builder->buffer[samsung_builder->cursor].level1 = samsung_builder->inverse;
+    samsung_builder->buffer[samsung_builder->cursor].duration1 = samsung_builder->repeat_code_low_ticks;
+    samsung_builder->cursor += 1;
+    samsung_builder_make_end(builder);
+    return ESP_OK;
+}
+
+static esp_err_t samsung_builder_get_result(ir_builder_t *builder, void *result, size_t *length)
+{
+    esp_err_t ret = ESP_OK;
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    SAMSUNG_CHECK(result && length, "result and length can't be null", err, ESP_ERR_INVALID_ARG);
+    *(rmt_item32_t **)result = samsung_builder->buffer;
+    *length = samsung_builder->cursor;
+    return ESP_OK;
+err:
+    return ret;
+}
+
+static esp_err_t samsung_builder_del(ir_builder_t *builder)
+{
+    samsung_builder_t *samsung_builder = __containerof(builder, samsung_builder_t, parent);
+    free(samsung_builder);
+    return ESP_OK;
+}
+
+ir_builder_t *ir_builder_rmt_new_samsung(const ir_builder_config_t *config)
+{
+    ir_builder_t *ret = NULL;
+    SAMSUNG_CHECK(config, "samsung configuration can't be null", err, NULL);
+    SAMSUNG_CHECK(config->buffer_size, "buffer size can't be zero", err, NULL);
+
+    uint32_t builder_size = sizeof(samsung_builder_t) + config->buffer_size * sizeof(rmt_item32_t);
+    samsung_builder_t *samsung_builder = calloc(1, builder_size);
+    SAMSUNG_CHECK(samsung_builder, "request memory for samsung_builder failed", err, NULL);
+
+    samsung_builder->buffer_size = config->buffer_size;
+    samsung_builder->flags = config->flags;
+    if (config->flags & IR_TOOLS_FLAGS_INVERSE) {
+        samsung_builder->inverse = true;
+    }
+
+    uint32_t counter_clk_hz = 0;
+    SAMSUNG_CHECK(rmt_get_counter_clock((rmt_channel_t)config->dev_hdl, &counter_clk_hz) == ESP_OK,
+              "get rmt counter clock failed", err, NULL);
+    float ratio = (float)counter_clk_hz / 1e6;
+    samsung_builder->leading_code_high_ticks = (uint32_t)(ratio * SAMSUNG_LEADING_CODE_HIGH_US);
+    samsung_builder->leading_code_low_ticks = (uint32_t)(ratio * SAMSUNG_LEADING_CODE_LOW_US);
+    samsung_builder->repeat_code_high_ticks = (uint32_t)(ratio * SAMSUNG_REPEAT_CODE_HIGH_US);
+    samsung_builder->repeat_code_low_ticks = (uint32_t)(ratio * SAMSUNG_REPEAT_CODE_LOW_US);
+    samsung_builder->payload_logic0_high_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ZERO_HIGH_US);
+    samsung_builder->payload_logic0_low_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ZERO_LOW_US);
+    samsung_builder->payload_logic1_high_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ONE_HIGH_US);
+    samsung_builder->payload_logic1_low_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ONE_LOW_US);
+    samsung_builder->ending_code_high_ticks = (uint32_t)(ratio * SAMSUNG_ENDING_CODE_HIGH_US);
+    samsung_builder->ending_code_low_ticks = 0x7FFF;
+    samsung_builder->parent.make_head = samsung_builder_make_head;
+    samsung_builder->parent.make_logic0 = samsung_builder_make_logic0;
+    samsung_builder->parent.make_logic1 = samsung_builder_make_logic1;
+    samsung_builder->parent.make_end = samsung_builder_make_end;
+    samsung_builder->parent.build_frame = samsung_build_frame;
+    samsung_builder->parent.build_repeat_frame = samsung_build_repeat_frame;
+    samsung_builder->parent.get_result = samsung_builder_get_result;
+    samsung_builder->parent.del = samsung_builder_del;
+    samsung_builder->parent.repeat_period_ms = 110;
+    return &samsung_builder->parent;
+err:
+    return ret;
+}
---
+++ components/infrared_tools/src/ir_parser_rmt_samsung.c
@@ -0,0 +1,220 @@
+// Copyright 2019 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <stdlib.h>
+#include <sys/cdefs.h>
+#include "esp_log.h"
+#include "ir_tools.h"
+#include "ir_timings.h"
+#include "driver/rmt.h"
+
+static const char *TAG = "samsung_parser";
+#define SAMSUNG_CHECK(a, str, goto_tag, ret_value, ...)                               \
+    do                                                                            \
+    {                                                                             \
+        if (!(a))                                                                 \
+        {                                                                         \
+            ESP_LOGE(TAG, "%s(%d): " str, __FUNCTION__, __LINE__, ##__VA_ARGS__); \
+            ret = ret_value;                                                      \
+            goto goto_tag;                                                        \
+        }                                                                         \
+    } while (0)
+
+#define SAMSUNG_DATA_FRAME_RMT_WORDS (34)
+#define SAMSUNG_REPEAT_FRAME_RMT_WORDS (2)
+
+typedef struct {
+    ir_parser_t parent;
+    uint32_t flags;
+    uint32_t leading_code_high_ticks;
+    uint32_t leading_code_low_ticks;
+    uint32_t repeat_code_high_ticks;
+    uint32_t repeat_code_low_ticks;
+    uint32_t payload_logic0_high_ticks;
+    uint32_t payload_logic0_low_ticks;
+    uint32_t payload_logic1_high_ticks;
+    uint32_t payload_logic1_low_ticks;
+    uint32_t margin_ticks;
+    uint32_t cursor;
+    uint32_t last_address;
+    uint32_t last_command;
+    bool repeat;
+    bool inverse;
+    rmt_item32_t *buffer;
+} samsung_parser_t;
+
+static inline bool samsung_check_in_range(uint32_t raw_ticks, uint32_t target_ticks, uint32_t margin_ticks)
+{
+    return (raw_ticks < (target_ticks + margin_ticks)) && (raw_ticks > (target_ticks - margin_ticks));
+}
+
+static bool samsung_parse_head(samsung_parser_t *samsung_parser)
+{
+    samsung_parser->cursor = 0;
+    rmt_item32_t item = samsung_parser->buffer[samsung_parser->cursor];
+    bool ret = (item.level0 == samsung_parser->inverse) && (item.level1 != samsung_parser->inverse) &&
+               samsung_check_in_range(item.duration0, samsung_parser->leading_code_high_ticks, samsung_parser->margin_ticks) &&
+               samsung_check_in_range(item.duration1, samsung_parser->leading_code_low_ticks, samsung_parser->margin_ticks);
+    samsung_parser->cursor += 1;
+    return ret;
+}
+
+static bool samsung_parse_logic0(samsung_parser_t *samsung_parser)
+{
+    rmt_item32_t item = samsung_parser->buffer[samsung_parser->cursor];
+    bool ret = (item.level0 == samsung_parser->inverse) && (item.level1 != samsung_parser->inverse) &&
+               samsung_check_in_range(item.duration0, samsung_parser->payload_logic0_high_ticks, samsung_parser->margin_ticks) &&
+               samsung_check_in_range(item.duration1, samsung_parser->payload_logic0_low_ticks, samsung_parser->margin_ticks);
+    return ret;
+}
+
+static bool samsung_parse_logic1(samsung_parser_t *samsung_parser)
+{
+    rmt_item32_t item = samsung_parser->buffer[samsung_parser->cursor];
+    bool ret = (item.level0 == samsung_parser->inverse) && (item.level1 != samsung_parser->inverse) &&
+               samsung_check_in_range(item.duration0, samsung_parser->payload_logic1_high_ticks, samsung_parser->margin_ticks) &&
+               samsung_check_in_range(item.duration1, samsung_parser->payload_logic1_low_ticks, samsung_parser->margin_ticks);
+    return ret;
+}
+
+static esp_err_t samsung_parse_logic(ir_parser_t *parser, bool *logic)
+{
+    esp_err_t ret = ESP_FAIL;
+    bool logic_value = false;
+    samsung_parser_t *samsung_parser = __containerof(parser, samsung_parser_t, parent);
+    if (samsung_parse_logic0(samsung_parser)) {
+        logic_value = false;
+        ret = ESP_OK;
+    } else if (samsung_parse_logic1(samsung_parser)) {
+        logic_value = true;
+        ret = ESP_OK;
+    }
+    if (ret == ESP_OK) {
+        *logic = logic_value;
+    }
+    samsung_parser->cursor += 1;
+    return ret;
+}
+
+
+static bool samsung_parse_repeat_frame(samsung_parser_t *samsung_parser)
+{
+    samsung_parser->cursor = 0;
+    rmt_item32_t item = samsung_parser->buffer[samsung_parser->cursor];
+    bool ret = (item.level0 == samsung_parser->inverse) && (item.level1 != samsung_parser->inverse) &&
+               samsung_check_in_range(item.duration0, samsung_parser->repeat_code_high_ticks, samsung_parser->margin_ticks) &&
+               samsung_check_in_range(item.duration1, samsung_parser->repeat_code_low_ticks, samsung_parser->margin_ticks);
+    samsung_parser->cursor += 1;
+    return ret;
+}
+
+
+static esp_err_t samsung_parser_input(ir_parser_t *parser, void *raw_data, uint32_t length)
+{
+    esp_err_t ret = ESP_OK;
+    samsung_parser_t *samsung_parser = __containerof(parser, samsung_parser_t, parent);
+    SAMSUNG_CHECK(raw_data, "input data can't be null", err, ESP_ERR_INVALID_ARG);
+    samsung_parser->buffer = raw_data;
+    // Data Frame costs 34 items and Repeat Frame costs 2 items
+    if (length == SAMSUNG_DATA_FRAME_RMT_WORDS) {
+        samsung_parser->repeat = false;
+    } else if (length == SAMSUNG_REPEAT_FRAME_RMT_WORDS) {
+        samsung_parser->repeat = true;
+    } else {
+        ret = ESP_FAIL;
+    }
+    return ret;
+err:
+    return ret;
+}
+
+static esp_err_t samsung_parser_get_scan_code(ir_parser_t *parser, uint32_t *address, uint32_t *command, bool *repeat)
+{
+    esp_err_t ret = ESP_FAIL;
+    uint32_t addr = 0;
+    uint32_t cmd = 0;
+    bool logic_value = false;
+    samsung_parser_t *samsung_parser = __containerof(parser, samsung_parser_t, parent);
+    SAMSUNG_CHECK(address && command && repeat, "address, command and repeat can't be null", out, ESP_ERR_INVALID_ARG);
+    if (samsung_parser->repeat) {
+        if (samsung_parse_repeat_frame(samsung_parser)) {
+            *address = samsung_parser->last_address;
+            *command = samsung_parser->last_command;
+            *repeat = true;
+            ret = ESP_OK;
+        }
+    } else {
+        if (samsung_parse_head(samsung_parser)) {
+            for (int i = 0; i < 16; i++) {
+                if (samsung_parse_logic(parser, &logic_value) == ESP_OK) {
+                    addr |= (logic_value << i);
+                }
+            }
+            for (int i = 0; i < 16; i++) {
+                if (samsung_parse_logic(parser, &logic_value) == ESP_OK) {
+                    cmd |= (logic_value << i);
+                }
+            }
+            *address = addr;
+            *command = cmd;
+            *repeat = false;
+            // keep it as potential repeat code
+            samsung_parser->last_address = addr;
+            samsung_parser->last_command = cmd;
+            ret = ESP_OK;
+        }
+    }
+out:
+    return ret;
+}
+
+static esp_err_t samsung_parser_del(ir_parser_t *parser)
+{
+    samsung_parser_t *samsung_parser = __containerof(parser, samsung_parser_t, parent);
+    free(samsung_parser);
+    return ESP_OK;
+}
+
+ir_parser_t *ir_parser_rmt_new_samsung(const ir_parser_config_t *config)
+{
+    ir_parser_t *ret = NULL;
+    SAMSUNG_CHECK(config, "samsung configuration can't be null", err, NULL);
+
+    samsung_parser_t *samsung_parser = calloc(1, sizeof(samsung_parser_t));
+    SAMSUNG_CHECK(samsung_parser, "request memory for samsung_parser failed", err, NULL);
+
+    samsung_parser->flags = config->flags;
+    if (config->flags & IR_TOOLS_FLAGS_INVERSE) {
+        samsung_parser->inverse = true;
+    }
+
+    uint32_t counter_clk_hz = 0;
+    SAMSUNG_CHECK(rmt_get_counter_clock((rmt_channel_t)config->dev_hdl, &counter_clk_hz) == ESP_OK,
+              "get rmt counter clock failed", err, NULL);
+    float ratio = (float)counter_clk_hz / 1e6;
+    samsung_parser->leading_code_high_ticks = (uint32_t)(ratio * SAMSUNG_LEADING_CODE_HIGH_US);
+    samsung_parser->leading_code_low_ticks = (uint32_t)(ratio * SAMSUNG_LEADING_CODE_LOW_US);
+    samsung_parser->repeat_code_high_ticks = (uint32_t)(ratio * SAMSUNG_REPEAT_CODE_HIGH_US);
+    samsung_parser->repeat_code_low_ticks = (uint32_t)(ratio * SAMSUNG_REPEAT_CODE_LOW_US);
+    samsung_parser->payload_logic0_high_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ZERO_HIGH_US);
+    samsung_parser->payload_logic0_low_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ZERO_LOW_US);
+    samsung_parser->payload_logic1_high_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ONE_HIGH_US);
+    samsung_parser->payload_logic1_low_ticks = (uint32_t)(ratio * SAMSUNG_PAYLOAD_ONE_LOW_US);
+    samsung_parser->margin_ticks = (uint32_t)(ratio * config->margin_us);
+    samsung_parser->parent.input = samsung_parser_input;
+    samsung_parser->parent.get_scan_code = samsung_parser_get_scan_code;
+    samsung_parser->parent.del = samsung_parser_del;
+    return &samsung_parser->parent;
+err:
+    return ret;
+}
